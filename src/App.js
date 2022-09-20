3d37c76f2b94855dfbac7d27070719f091664c48
SidorovAA <Sidorovsansanih@gmail.com>
2022-09-20 17:26:09 +0300

    add lesson1

---
+++ src/App.js
@@ -0,0 +1,21 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+   const massege = 'Привет МИР!!!'
+  return (
+        <div className='App'>
+          <Mymassege sms = {massege}/>
+        </div>
+  );
+}
+
+export default App;
+const Mymassege = (props) => {
+return (
+<div className = "stile-form">
+  <h1 className = "stile-text"> Я на связи. {props.sms}</h1>
+</div>
+)
+  
+}
