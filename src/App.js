f849cc052deef8c178e9d8e8837f5db01481f2bc
SidorovAA <Sidorovsansanih@gmail.com>
2022-09-20 17:38:23 +0300

    correct lesson1 app.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-// import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
-   const massege = 'Привет МИР!!!'
+  const massege = 'Привет МИР!!!'
   return (
-        <div className='App'>
-          <Mymassege sms = {massege}/>
-        </div>
+    <div className='App'>
+      <Mymassege sms={massege} />
+    </div>
   );
 }
 
 export default App;
 const Mymassege = (props) => {
-return (
-<div className = "stile-form">
-  <h1 className = "stile-text"> Я на связи. {props.sms}</h1>
-</div>
-)
-  
+  return (
+    <div className="stile-form">
+      <h1 className="stile-text"> Я на связи. {props.sms}</h1>
+    </div>
+  )
+
 }
